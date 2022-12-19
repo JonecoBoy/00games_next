7ddda74adb373cb388652e69e8454ead48540e8f
joneco <joneco@hotmail.com>
2022-12-19 00:43:55 -0300

    none

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../src/layout/Layout'
-import Message from './_message'
 import { ApolloProvider } from '@apollo/client'
 import { apolloClient } from '../src/apolloClient'
 import { SessionProvider } from 'next-auth/react'
@@ -11,7 +10,6 @@ export default function App({ Component, pageProps:{session,...pageProps} }: App
     <SessionProvider session={session}>
     <ApolloProvider client={apolloClient}>
       <Layout>
-        <Message/>
         <Component {...pageProps} />
         <style jsx global>{`
 
